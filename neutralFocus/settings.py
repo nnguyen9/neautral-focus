090a0e23a5c475cdbe1ae34ebbef9e8f0b44fd3f
Nam Nguyen <nnguyen9@vt.edu>
2017-03-30 16:12:27 -0400

    Add STATIC_ROOT

--- neutralFocus/settings.py
+++ neutralFocus/settings.py
@@ -115,6 +115,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
